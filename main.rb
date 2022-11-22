8026b21e93b3e614980326f66cb1e66271ecf57b
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-11-22 09:23:08 +0000

    Add main.rb file

---
+++ main.rb
@@ -0,0 +1,34 @@
+
+class Main
+
+  def start
+    puts 'Welcome to the App'
+    loop do
+      list_options
+      option = gets.chomp.to_i
+      if option == 13
+        puts 'Goodbye'
+        break
+      end
+    end
+  end
+
+  def list_options
+    puts '1. List all books'
+    puts '2. List all labels'
+    puts '3. Add a book'
+    puts '4. List all music albums'
+    puts '5. List all genres '
+    puts '6. Add a music album'
+    puts '7. List of games'
+    puts '8. List all authors'
+    puts '9. Add a game'
+    puts '10. List all movies'
+    puts '11. List all sources'
+    puts '12. Add a movie'
+    puts '13. Exit'
+  end
+end
+
+main = Main.new
+main.start
