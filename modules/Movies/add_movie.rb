eacc7ff4fd0aca2d8bc0e812ddabf51b49af1b28
benjamin kisenge <benkisenge03@gmail.com>
2022-11-25 11:39:21 +0200

    Create module for movie and source method

---
+++ modules/Movies/add_movie.rb
@@ -0,0 +1,17 @@
+require_relative '../movie'
+require_relative '../source'
+
+module AddMovie
+    def add_movie
+        print 'Published date: '
+        publish_date = gets.chomp()
+        print 'Movie is silent? [y/n]: '
+        silent = gets.chomp.upcase == 'y' || 'Y'
+        print 'Source name: '
+        source_name = gets.chomp()
+        source = Source.new(source_name)
+        movie = Movie.new(source, publish_date, silent)
+        @movies << movie
+        puts 'Movie added successfully'
+    end
+end
