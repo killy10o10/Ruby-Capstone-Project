2bcad66ec3c16e18b1d8d13ade1855997e2a5510
benjamin kisenge <benkisenge03@gmail.com>
2022-11-25 11:36:54 +0200

    Modify item class

--- item.rb
+++ item.rb
@@ -4,15 +4,15 @@ class Item
   attr_accessor :publish_date, :author, :source, :genre, :label, :id
   attr_reader :archived
 
-  def initialize(publish_date)
+  def initialize(publish_date,archived: false)
     @id = Random.rand(1..1000)
-    @publish_date = Date.parse(publish_date)
-    @archived = false
-    # 1-to-many relationships
-    @author = 'unknown'
-    @source = 'unknown'
-    @label = 'unknown'
-    @genre = 'unknown'
+    @publish_date = publish_date
+    @archived = archived
+    # # 1-to-many relationships
+    # @author = author
+    # @source = source
+    # @label = label
+    # @genre = genre
   end
 
   def add_author(author)
@@ -20,12 +20,21 @@ class Item
     author.add_item(self) unless author.items.include?(self)
   end
 
-  def can_be_archived?
-    ten_years_ago = time.now.year - @publish_date.year
-    ten_years_ago > 10
+  def add_source(source)
+    @source = source
   end
 
   def move_to_archive
     @archived = true if can_be_archived?
   end
+
+  private
+
+  def can_be_archived?
+    Date.today.year - @publish_date.to_i > 10
+  end
 end
+
+
+# i = Item.new('2003')
+# puts i.can_be_archived?
