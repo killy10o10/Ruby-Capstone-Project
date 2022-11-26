48c32632a650505d53101722680258ab23b0006f
aeh1707d <abdelhadi.hireche@outlook.com>
2022-11-26 07:44:44 +0100

    Fix linter errors

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -4,17 +4,17 @@ require_relative '../sources/source'
 describe Movie do
   context 'When create a new movie' do
     it 'return a new object' do
-      source = Source.new("Netflix")
+      source = Source.new('Netflix')
       movie = Movie.new(source, '2022', true)
       expect(movie).to be_an_instance_of(Movie)
     end
     it 'return the correct silet' do
-      source = Source.new("Netflix")
+      source = Source.new('Netflix')
       movie = Movie.new(source, '2022', true)
       expect(movie.silent).to eql(true)
     end
     it 'return the possiblity of archiving the item ' do
-      source = Source.new("Netflix")
+      source = Source.new('Netflix')
       movie = Movie.new(source, '2022-08-06', false)
       expect(movie.can_be_archived?).to eql(false)
     end
